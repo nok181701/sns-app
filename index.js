bf2cfdff814736692423739e5148c3ba72201130
祝井直樹 <nok181701@gmail.com>
2023-12-11 16:47:28 +0900

    ✨ Mongo DB 接続

--- index.js
+++ index.js
@@ -3,6 +3,18 @@ const app = express();
 const userRouter = require("./routes/users");
 const authRouter = require("./routes/auth");
 const postRouter = require("./routes/posts");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+//DB 接続
+mongoose
+  .connect(process.env.MONGOURL)
+  .then(() => {
+    console.log("DBと接続中...");
+  })
+  .catch((err) => {
+    console.log(`エラー：${err}`);
+  });
 
 //Middleware
 app.use("/api/auth", authRouter);
