c10c359e19293cedba0a4310a9f6f047b9a15561
祝井直樹 <nok181701@gmail.com>
2023-12-11 20:51:19 +0900

    ✨ ユーザー情報取得のAPI作成

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,17 @@
 const router = require("express").Router();
 const User = require("../models/User");
 
+//ユーザー情報取得
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, updatedAt, ...other } = user._doc;
+    res.status(200).json(other);
+  } catch (err) {
+    return res.status(500).json(`取得できませんでした：${err}`);
+  }
+});
+
 //ユーザー情報更新
 router.put("/:id", async (req, res) => {
   if (req.body.userId === req.params.id || req.body.isAdomin) {
@@ -30,9 +41,5 @@ router.delete("/:id", async (req, res) => {
     return res.status(403).json("自分のアカウントのみ削除できます。");
   }
 });
-
-// router.get("/", (req, res) => {
-//   res.send("Hello");
-// });
 
 module.exports = router;
