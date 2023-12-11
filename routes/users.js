dc58bd1b78559aab712af0e0ecc95cae640fddad
祝井直樹 <nok181701@gmail.com>
2023-12-11 19:57:22 +0900

    ✨ ユーザー情報更新のAPI作成

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,21 @@
 const router = require("express").Router();
+const User = require("../models/User");
+
+//ユーザー情報更新
+router.put("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdomin) {
+    try {
+      const user = await User.findByIdAndUpdate(req.params.id, {
+        $set: req.body,
+      });
+      return res.status(200).json("ユーザー情報が更新されました。");
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res.status(403).json("自分のアカウントのみ更新できます。");
+  }
+});
 
 // router.get("/", (req, res) => {
 //   res.send("Hello");
