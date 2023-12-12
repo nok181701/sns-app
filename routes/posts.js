7bde33e3b02ce8220a69770b67f8f26d144d2a77
祝井直樹 <nok181701@gmail.com>
2023-12-12 15:44:54 +0900

    ✨ 特定の投稿を取得・いいね・タイムライン表示の実装

--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const Post = require("../models/Post");
+const User = require("../models/User");
 
 //投稿の作成
 router.post("/", async (req, res) => {
@@ -41,6 +42,56 @@ router.delete("/:id", async (req, res) => {
     }
   } catch (err) {
     return res.status(403).json(err);
+  }
+});
+
+//特定の投稿を取得
+router.get("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    return res.status(200).json(post);
+  } catch (err) {
+    return res.status(403).json(err);
+  }
+});
+
+//特定の投稿にいいねを押す
+router.put("/:id/like", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post.likes.includes(req.body.userId)) {
+      await post.updateOne({
+        $push: {
+          likes: req.body.userId,
+        },
+      });
+      return res.status(200).json("いいねをしました");
+    } else {
+      await post.updateOne({
+        $pull: {
+          likes: req.body.userId,
+        },
+      });
+      return res.status(403).json("いいねを外しました");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//タイムラインの投稿を取得
+router.get("/timeline/all", async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.body.userId);
+    const userPosts = await Post.find({ userId: currentUser._id });
+    const friendsPosts = await Promise.all(
+      currentUser.followings.map((frinendId) => {
+        return Post.find({ userId: frinendId });
+      })
+    );
+    return res.status(200).json(userPosts.concat(...friendsPosts));
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
