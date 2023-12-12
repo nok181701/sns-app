72d3dbff0757b092dd2c063744b2487a35a27ba1
祝井直樹 <nok181701@gmail.com>
2023-12-12 13:06:23 +0900

    ✨ ユーザーのフォロー/フォロー解除の実装

--- routes/users.js
+++ routes/users.js
@@ -19,12 +19,12 @@ router.put("/:id", async (req, res) => {
       const user = await User.findByIdAndUpdate(req.params.id, {
         $set: req.body,
       });
-      return res.status(200).json("ユーザー情報が更新されました。");
+      return res.status(200).json("ユーザー情報が更新されました");
     } catch (err) {
       return res.status(500).json(err);
     }
   } else {
-    return res.status(403).json("自分のアカウントのみ更新できます。");
+    return res.status(403).json("自分のアカウントのみ更新できます");
   }
 });
 
@@ -33,12 +33,70 @@ router.delete("/:id", async (req, res) => {
   if (req.body.userId === req.params.id || req.body.isAdomin) {
     try {
       await User.findByIdAndDelete(req.params.id);
-      return res.status(200).json("ユーザー情報が削除されました。");
+      return res.status(200).json("ユーザー情報が削除されました");
     } catch (err) {
       return res.status(500).json(err);
     }
   } else {
-    return res.status(403).json("自分のアカウントのみ削除できます。");
+    return res.status(403).json("自分のアカウントのみ削除できます");
+  }
+});
+
+// ユーザーのフォロー
+router.put("/:id/follow", async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      const curUser = await User.findById(req.body.userId);
+      if (!user.followers.includes(req.body.userId)) {
+        await user.updateOne({
+          $push: {
+            followers: req.body.userId,
+          },
+        });
+        await curUser.updateOne({
+          $push: {
+            followings: req.params.id,
+          },
+        });
+        return res.status(200).json("フォローしました");
+      } else {
+        return res.status(403).json("すでにこのユーザーをフォローしています");
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(500).json("自分自身はフォローできません");
+  }
+});
+
+// ユーザーのフォロー解除
+router.put("/:id/unfollow", async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      const curUser = await User.findById(req.body.userId);
+      if (user.followers.includes(req.body.userId)) {
+        await user.updateOne({
+          $pull: {
+            followers: req.body.userId,
+          },
+        });
+        await curUser.updateOne({
+          $pull: {
+            followings: req.params.id,
+          },
+        });
+        return res.status(200).json("フォローを解除しました");
+      } else {
+        return res.status(403).json("このユーザをフォロー解除できません");
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(500).json("自分自身はフォロー解除できません");
   }
 });
 
