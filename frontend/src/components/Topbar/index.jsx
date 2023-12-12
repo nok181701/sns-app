d3b7cc7802843e9c0a4e897d1fe239ecc4fc0f85
祝井直樹 <nok181701@gmail.com>
2023-12-12 17:20:58 +0900

    🍱 画像の追加

--- frontend/src/components/Topbar/index.jsx
+++ frontend/src/components/Topbar/index.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 
 const Topbar = () => {
-  return <div>Topbarだよ</div>;
+  return (
+    <div className="topbarConteiner">
+      <div className="topbarLeft">
+        <span className="logo">SNS APP</span>
+      </div>
+      <div className="topbarCenter">
+        <div className="serchbar">
+          <input
+            type="text"
+            className="serchInput"
+            placeholder="探し物はなんですか？"
+          />
+        </div>
+        <div className="topbarRight">
+          <div className="topbarIconItem">1</div>
+          <div className="topbarIconItem">2</div>
+        </div>
+        <img src="/assets/person/1.jpeg" alt="" className="topbarImg" />
+      </div>
+    </div>
+  );
 };
 
 export default Topbar;
