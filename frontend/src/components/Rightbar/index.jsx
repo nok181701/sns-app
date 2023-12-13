55eb97e87b63b748281505fc50f67323f17d7d7a
祝井直樹 <nok181701@gmail.com>
2023-12-13 16:21:09 +0900

    💄 先に比率だけスタイルを更新

---
+++ frontend/src/components/Rightbar/index.jsx
@@ -0,0 +1,10 @@
+import "src/components/Rightbar/Rightbar.css";
+const Rirhtbar = () => {
+  return (
+    <>
+      <div className="rightbar">Rightbar</div>
+    </>
+  );
+};
+
+export default Rirhtbar;
