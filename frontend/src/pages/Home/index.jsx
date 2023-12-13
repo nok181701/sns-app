51dc831fdd82c7db3fbc5de549bb1aaebfddb547
祝井直樹 <nok181701@gmail.com>
2023-12-13 16:42:13 +0900

    🚚 パス名変更

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -2,7 +2,7 @@ import Rirhtbar from "src/components/Rightbar";
 import Sidebar from "src/components/Sidebar";
 import TimeLine from "src/components/TimeLine";
 import Topbar from "src/components/Topbar";
-import "src/pages/Home";
+import "src/pages/Home/Home.css";
 const Home = () => {
   return (
     <>
