3ed97bdd752983b2b87bc95c7fee599d58fd6bf9
祝井直樹 <nok181701@gmail.com>
2023-12-14 14:27:48 +0900

    ✨ ライトバー実装

--- frontend/src/components/Rightbar/index.jsx
+++ frontend/src/components/Rightbar/index.jsx
@@ -2,7 +2,61 @@ import "src/components/Rightbar/Rightbar.css";
 const Rirhtbar = () => {
   return (
     <>
-      <div className="rightbar">Rightbar</div>
+      <div className="rightbar">
+        <div className="rightbarWrapper">
+          <div className="eventContainer">
+            <img src="assets/star.png" alt="イベント" className="starImg" />
+            <span className="eventText">
+              <b>フォロワー</b>限定イベント開催中
+            </span>
+          </div>
+          <img src="assets/ad.jpeg" alt="イベント画像" className="eventImg" />
+          <h4 className="rightbarTitle">オンラインの友達</h4>
+          <ul className="rightbarFriendList">
+            <li className="rightbarFriend">
+              <div className="rightbarProfileImgContainer">
+                <img
+                  src="assets/person/1.jpeg"
+                  alt="友達"
+                  className="rightbarProfileImg"
+                />
+                <span className="rightbarOnline"></span>
+              </div>
+              <span className="rightbarUserName">yamada</span>
+            </li>
+            <li className="rightbarFriend">
+              <div className="rightbarProfileImgContainer">
+                <img
+                  src="assets/person/2.jpeg"
+                  alt="友達"
+                  className="rightbarProfileImg"
+                />
+                <span className="rightbarOnline"></span>
+              </div>
+              <span className="rightbarUserName">Kevin</span>
+            </li>
+          </ul>
+          <p className="promotionTitle">プロモーション広告</p>
+          <img
+            src="assets/promotion/promotion1.jpeg"
+            alt="プロモーション画像 "
+            className="rightbarPromotionImg"
+          />
+          <p className="promotionName">ショッピング</p>
+          <img
+            src="assets/promotion/promotion2.jpeg"
+            alt="プロモーション画像 "
+            className="rightbarPromotionImg"
+          />
+          <p className="promotionName">カーショップ</p>{" "}
+          <img
+            src="assets/promotion/promotion3.jpeg"
+            alt="プロモーション画像 "
+            className="rightbarPromotionImg"
+          />
+          <p className="promotionName">X株式会社</p>
+        </div>
+      </div>
     </>
   );
 };
