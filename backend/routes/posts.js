331fa742787be01ace79af595a3723536bf81c16
祝井直樹 <nok181701@gmail.com>
2023-12-16 13:11:43 +0900

    🩹 エンドポイントと記述もれの修正

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -80,9 +80,9 @@ router.put("/:id/like", async (req, res) => {
 });
 
 //タイムラインの投稿を取得
-router.get("/timeline/all", async (req, res) => {
+router.get("/timeline/:userId", async (req, res) => {
   try {
-    const currentUser = await User.findById(req.body.userId);
+    const currentUser = await User.findById(req.params.userId);
     const userPosts = await Post.find({ userId: currentUser._id });
     const friendsPosts = await Promise.all(
       currentUser.followings.map((frinendId) => {
