7e2b3134c660325feed17a13c7b9cf47744612ab
祝井直樹 <nok181701@gmail.com>
2023-12-17 19:06:49 +0900

    ✨ グローバルコンテキストの変数を用いてログインor新規登録ページの遷移

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,16 +2,30 @@ import Profile from "src/pages/Profile";
 import Home from "./pages/Home";
 import Login from "src/pages/Login";
 import Register from "src/pages/Register";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "./state/AuthContext";
 
 function App() {
+  const { user } = useContext(AuthContext);
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
+          <Route path="/" element={user ? <Home /> : <Register />} />
+          <Route
+            path="/login"
+            element={user ? <Navigate to="/" /> : <Login />}
+          />
+          <Route
+            path="/register"
+            element={user ? <Navigate to="/" /> : <Register />}
+          />
           <Route path="/profile/:username" element={<Profile />} />
         </Routes>
       </Router>
