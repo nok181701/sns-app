834792effce8df2a901688eb383569d9f10f32a4
祝井直樹 <nok181701@gmail.com>
2023-12-17 11:24:56 +0900

    ✨ クエリでユーザー情報取得API作成

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -12,6 +12,21 @@ router.get("/:id", async (req, res) => {
   }
 });
 
+//クエリでユーザー情報取得
+router.get("/", async (req, res) => {
+  const userId = req.query.userId;
+  const username = req.query.username;
+  try {
+    const user = userId
+      ? await User.findById(userId)
+      : await User.findOne({ username });
+    const { password, updatedAt, ...other } = user._doc;
+    return res.status(200).json(other);
+  } catch (err) {
+    return res.status(500).json(`取得できませんでした：${err}`);
+  }
+});
+
 //ユーザー情報更新
 router.put("/:id", async (req, res) => {
   if (req.body.userId === req.params.id || req.body.isAdomin) {
