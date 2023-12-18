8ebe04d59cd159d9892bb8cde4f27a00e0b75439
祝井直樹 <nok181701@gmail.com>
2023-12-18 13:34:17 +0900

    ✨ 画像投稿の下準備

--- frontend/src/components/Share/index.jsx
+++ frontend/src/components/Share/index.jsx
@@ -1,11 +1,18 @@
 import "src/components/Share/Share.css";
 import { Analytics, Face, Gif, Image } from "@mui/icons-material";
-import { useContext, useRef } from "react";
+import { useContext, useRef, useState } from "react";
 import { AuthContext } from "src/state/AuthContext";
 import axios from "axios";
 
 const SHAREOPTIONS = [
-  { img: () => <Image htmlColor="#2c517c" />, text: "写真", color: "red" },
+  {
+    img: () => <Image htmlColor="#2c517c" />,
+    text: "写真",
+    color: "red",
+    type: "file",
+    id: "file",
+    accept: ".png, .jpeg, .jpg",
+  },
   { img: () => <Gif htmlColor="#a380c4" />, text: "Gif", color: "red" },
   { img: () => <Face htmlColor="#d69b85" />, text: "気持ち", color: "red" },
   { img: () => <Analytics htmlColor="#d98080" />, text: "投票", color: "red" },
@@ -14,6 +21,7 @@ const Share = () => {
   const PUBLIC_FOLDER = process.env.REACT_APP_PUBLIC_FOLDER;
   const { user } = useContext(AuthContext); //ログインしているユーザー
   const desc = useRef();
+  const [file, setFile] = useState(null);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -56,10 +64,19 @@ const Share = () => {
               {SHAREOPTIONS.map((option, index) => {
                 const OptionImg = option.img;
                 return (
-                  <div className="shareOption" key={index}>
+                  <label className="shareOption" key={index} htmlFor="file">
                     {OptionImg && <OptionImg className="shareIcon" />}
                     <span className="shareOptionText">{option.text}</span>
-                  </div>
+                    {option.type === "file" && (
+                      <input
+                        type={option.type}
+                        id={option.id}
+                        accept={option.accept}
+                        style={{ display: "none" }}
+                        onChange={(e) => setFile(e.target.files[0])}
+                      />
+                    )}
+                  </label>
                 );
               })}
             </div>
