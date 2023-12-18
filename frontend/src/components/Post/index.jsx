c511d01493e6e0a47a25774a44c4dfca64ccae77
祝井直樹 <nok181701@gmail.com>
2023-12-18 13:05:23 +0900

    ✨ いいね機能の実装

--- frontend/src/components/Post/index.jsx
+++ frontend/src/components/Post/index.jsx
@@ -1,17 +1,24 @@
 import "src/components/Post/Post.css";
 import axios from "axios";
 import { MoreVert } from "@mui/icons-material";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { format } from "timeago.js";
 import { Link } from "react-router-dom";
+import { AuthContext } from "src/state/AuthContext";
 
 const Post = ({ post }) => {
   const PUBLIC_FOLDER = process.env.REACT_APP_PUBLIC_FOLDER;
   const [like, setLike] = useState(post.likes.length);
   const [isLiked, setIsLiked] = useState(false);
-  const [user, setUser] = useState({});
+  const [user, setUser] = useState({}); //投稿したuser
+  const { user: currentUser } = useContext(AuthContext); //ログインしているユーザー
 
-  const handleLike = () => {
+  const handleLike = async () => {
+    try {
+      await axios.put(`/posts/${post._id}/like`, { userId: currentUser._id });
+    } catch (err) {
+      console.log(err);
+    }
     setLike((prevLike) => (isLiked ? prevLike - 1 : prevLike + 1));
     setIsLiked(!isLiked);
   };
@@ -33,8 +40,9 @@ const Post = ({ post }) => {
               <Link to={`/profile/${user.username}`}>
                 <img
                   src={
-                    user.profilePicture ||
-                    PUBLIC_FOLDER + "/person/noAvatar.png"
+                    user.profilePicture
+                      ? PUBLIC_FOLDER + user.profilePicture
+                      : PUBLIC_FOLDER + "/person/noAvatar.png"
                   }
                   alt="プロフィール画像"
                   className="postProfileImg"
