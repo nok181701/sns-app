5c2a8fdd686d111d81db76c6f0a7e89b884a812c
祝井直樹 <nok181701@gmail.com>
2023-12-18 13:09:01 +0900

    🩹 初期値のprofilePictureに値を設定

--- frontend/src/state/AuthContext.jsx
+++ frontend/src/state/AuthContext.jsx
@@ -3,7 +3,17 @@ import AuthReducer from "./AuthReducer";
 
 //最初のユーザー定義
 const initialState = {
-  user: null,
+  // user: null,
+  user: {
+    _id: "657ec55aa1cbfbf43c8225a8",
+    username: "にゃーん",
+    email: "cat@gmail.com",
+    password: "$2b$10$3Br4Lq3lFDYOQOIFfIwTeOABSkJ7H0.FQtGsQzbx6mXt6og3oMyeu",
+    profilePicture: "/person/cat.jpeg",
+    coverPicture: "",
+    followers: [],
+    followings: [],
+  },
   isFetching: false,
   error: false,
 };
