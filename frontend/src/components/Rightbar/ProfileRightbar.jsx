beda917ec19b6d219dbe11c9ddc56cfa3be52898
祝井直樹 <nok181701@gmail.com>
2023-12-19 22:03:15 +0900

    ✨ ユーザーをフォローしている人欄実装

--- frontend/src/components/Rightbar/ProfileRightbar.jsx
+++ frontend/src/components/Rightbar/ProfileRightbar.jsx
@@ -1,48 +1,53 @@
 import "src/components/Rightbar/ProfileRightbar.css";
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "src/state/AuthContext";
+import axios from "axios";
+
 const ProfileRightbar = () => {
   const PUBLIC_FOLDER = process.env.REACT_APP_PUBLIC_FOLDER;
+  const { user } = useContext(AuthContext);
+  const [closeFriends, setCloseFriends] = useState([]);
+
+  useEffect(() => {
+    const getCloseFriends = async () => {
+      if (user.followers && user.followers.length > 0) {
+        const closeFriendsPromises = user.followers.map(async (followerId) => {
+          const response = await axios.get(`/users/${followerId}`);
+          return response.data;
+        });
+        const friendsData = await Promise.all(closeFriendsPromises);
+        setCloseFriends(friendsData);
+      }
+    };
+    getCloseFriends();
+  }, [user.followers]);
+
   return (
     <>
       <h4 className="rightbarTitle">ユーザー情報</h4>
       <div className="rightbarInfo">
         <div className="rightbarInfoItem">
           <span className="rightbarInfoKey">出身:</span>
-          <span className="rightbarInfoKey">東京</span>
+          <span className="rightbarInfoKey">{user.city}</span>
         </div>
-        <h4 className="rightbarTitle">あなたの友達</h4>
+        <h4 className="rightbarTitle">あなたをフォローしている人</h4>
         <div className="rightbarFollowings">
-          <div className="rightbarFollowing">
-            <img
-              src={PUBLIC_FOLDER + "/person/1.jpeg"}
-              alt=""
-              className="rightbarFollowingImg"
-            />
-            <span className="rightbarFollowingName">ShinCode</span>
-          </div>
-          <div className="rightbarFollowing">
-            <img
-              src={PUBLIC_FOLDER + "/person/2.jpeg"}
-              alt=""
-              className="rightbarFollowingImg"
-            />
-            <span className="rightbarFollowingName">Yamaki</span>
-          </div>
-          <div className="rightbarFollowing">
-            <img
-              src={PUBLIC_FOLDER + "/person/3.jpeg"}
-              alt=""
-              className="rightbarFollowingImg"
-            />
-            <span className="rightbarFollowingName">Koga</span>
-          </div>
-          <div className="rightbarFollowing">
-            <img
-              src={PUBLIC_FOLDER + "/person/4.jpeg"}
-              alt=""
-              className="rightbarFollowingImg"
-            />
-            <span className="rightbarFollowingName">Matukubo</span>
-          </div>
+          {closeFriends.map((friend) => {
+            return (
+              <div key={friend._id} className="rightbarFollowing">
+                <img
+                  src={
+                    friend.profilePicture
+                      ? PUBLIC_FOLDER + user.profilePicture
+                      : PUBLIC_FOLDER + "/person/noAvatar.png"
+                  }
+                  alt=""
+                  className="rightbarFollowingImg"
+                />
+                <span className="rightbarFollowingName">{friend.username}</span>
+              </div>
+            );
+          })}
         </div>
       </div>
     </>
