611e4d47c35fe03503b6261557bf1949cdc8dd08
祝井直樹 <nok181701@gmail.com>
2023-12-19 10:40:51 +0900

    ✨ upload router作成

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,9 @@ const app = express();
 const userRouter = require("./routes/users");
 const authRouter = require("./routes/auth");
 const postRouter = require("./routes/posts");
+const uploadRouter = require("./routes/upload");
 const mongoose = require("mongoose");
+const path = require("path");
 require("dotenv").config();
 
 //DB 接続
@@ -17,10 +19,12 @@ mongoose
   });
 
 //Middleware
+app.use("/images", express.static(path.join(__dirname, "public/images")));
 app.use(express.json());
 app.use("/api/auth", authRouter);
 app.use("/api/users", userRouter);
 app.use("/api/posts", postRouter);
+app.use("/api/upload", uploadRouter);
 
 app.listen(5000, () => {
   console.log("ポート3000で受付中...");
