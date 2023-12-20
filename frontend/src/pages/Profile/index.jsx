b153eeb3409d0026017473902960808474910468
祝井直樹 <nok181701@gmail.com>
2023-12-20 12:11:20 +0900

    ✨ プロフィールにログアウトボタン実装

--- frontend/src/pages/Profile/index.jsx
+++ frontend/src/pages/Profile/index.jsx
@@ -5,7 +5,7 @@ import TimeLine from "src/components/TimeLine";
 import Topbar from "src/components/Topbar";
 import axios from "axios";
 import React, { useContext, useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { AuthContext } from "src/state/AuthContext";
 
 const Profile = () => {
@@ -13,6 +13,10 @@ const Profile = () => {
   const { user: currentUser } = useContext(AuthContext);
   const [user, setUser] = useState({});
   const username = useParams().username;
+  const navigate = useNavigate();
+  const GoToLoginPage = () => {
+    navigate("/register");
+  };
 
   useEffect(() => {
     const fetchUser = async () => {
@@ -28,42 +32,48 @@ const Profile = () => {
 
   return (
     <>
-      <Topbar />
-      <div className="profile">
-        <Sidebar />
-        <div className="profileRight">
-          <div className="profileRightTop">
-            <div className="profileCover">
-              <img
-                src={user.coverPicture || PUBLIC_FOLDER + "/post/3.jpeg"}
-                alt="背景画像"
-                className="profileCoverImg"
-              />
-              <img
-                src={
-                  user.profilePicture
-                    ? PUBLIC_FOLDER + user.profilePicture
-                    : PUBLIC_FOLDER + "/person/noAvatar.png"
-                }
-                alt="プロフィール画像"
-                className="profileUserImg"
-              />
-            </div>
-            <div className="profileInfo">
-              <h4 className="profileInfoName">{user.username}</h4>
-              <span className="prfileInfoDesc">{user.desc}</span>
-              <div className="followWrapper">
-                <span className="followings">{`フォロー：${currentUser.followings.length}`}</span>
-                <span className="followers">{`フォロワー：${currentUser.followers.length}`}</span>
+      {currentUser ? (
+        <div>
+          <Topbar />
+          <div className="profile">
+            <Sidebar />
+            <div className="profileRight">
+              <div className="profileRightTop">
+                <div className="profileCover">
+                  <img
+                    src={user.coverPicture || PUBLIC_FOLDER + "/post/3.jpeg"}
+                    alt="背景画像"
+                    className="profileCoverImg"
+                  />
+                  <img
+                    src={
+                      user.profilePicture
+                        ? PUBLIC_FOLDER + "/" + user.profilePicture
+                        : PUBLIC_FOLDER + "/person/noAvatar.png"
+                    }
+                    alt="プロフィール画像"
+                    className="profileUserImg"
+                  />
+                </div>
+                <div className="profileInfo">
+                  <h4 className="profileInfoName">{user.username}</h4>
+                  <span className="prfileInfoDesc">{user.desc}</span>
+                  <div className="followWrapper">
+                    <span className="followings">{`フォロー：${currentUser.followings.length}`}</span>
+                    <span className="followers">{`フォロワー：${currentUser.followers.length}`}</span>
+                  </div>
+                </div>
+              </div>
+              <div className="profileRightBottom">
+                <TimeLine username={username} />
+                <Rirhtbar user={user} />
               </div>
             </div>
           </div>
-          <div className="profileRightBottom">
-            <TimeLine username={username} />
-            <Rirhtbar user={user} />
-          </div>
         </div>
-      </div>
+      ) : (
+        GoToLoginPage()
+      )}
     </>
   );
 };
