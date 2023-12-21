cb68335a1a9f9828b298b41b102220b2ebdb1995
祝井直樹 <nok181701@gmail.com>
2023-12-21 15:35:25 +0900

    🎨 細かなコードの修正

--- frontend/src/state/AuthAction.jsx
+++ frontend/src/state/AuthAction.jsx
@@ -1,15 +1,18 @@
-//ユーザー入力に応じたアクションの設定
-export const LoginStart = (user) => {
-  type: "LOGIN_START";
-};
-export const LoginSuccess = (user) => {
-  type: "LOGIN_SUCCESS";
-  payload: user;
-};
-export const LoginError = (error) => {
-  type: "LOGIN_ERROR";
-  payload: error;
-};
-export const LogoutAction = () => {
-  type: "LOGOUT";
-};
+// ユーザー入力に応じたアクションの設定
+export const LoginStart = (user) => ({
+  type: "LOGIN_START",
+});
+
+export const LoginSuccess = (user) => ({
+  type: "LOGIN_SUCCESS",
+  payload: user,
+});
+
+export const LoginError = (error) => ({
+  type: "LOGIN_ERROR",
+  payload: error,
+});
+
+export const LogoutAction = () => ({
+  type: "LOGOUT",
+});
