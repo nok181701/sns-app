8a2a0bb58a5e76534c693143fea25074769d9de7
祝井直樹 <nok181701@gmail.com>
2024-01-01 15:37:14 +0900

    🩹 パス修正

--- frontend/src/actionCalls.js
+++ frontend/src/actionCalls.js
@@ -3,7 +3,10 @@ import axios from "axios";
 export const loginCall = async (user, dispatch) => {
   dispatch({ type: "LOGIN_START" });
   try {
-    const res = await axios.post("auth/login", user);
+    const res = await axios.post(
+      "https://sns-app-backend-013cef86da2c.herokuapp.com/api/auth/login",
+      user
+    );
     dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
   } catch (err) {
     dispatch({ type: "LOGIN_ERROR", payload: err });
