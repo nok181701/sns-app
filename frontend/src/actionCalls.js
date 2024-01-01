2ec849eb73d05396b8cc6ce893f89857ac77eb79
祝井直樹 <nok181701@gmail.com>
2024-01-01 16:18:03 +0900

    🔒️ 環境変数追加

--- frontend/src/actionCalls.js
+++ frontend/src/actionCalls.js
@@ -1,12 +1,11 @@
 import axios from "axios";
+const apiUrl =
+  process.env.REACT_APP_API_URL || process.env.REACT_APP_BACKEND_ENDPOINT_DEV;
 
 export const loginCall = async (user, dispatch) => {
   dispatch({ type: "LOGIN_START" });
   try {
-    const res = await axios.post(
-      `${process.env.REACT_APP_BACKEND_ENDPOINT}/auth/login`,
-      user
-    );
+    const res = await axios.post(`${apiUrl}/auth/login`, user);
     dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
   } catch (err) {
     dispatch({ type: "LOGIN_ERROR", payload: err });
